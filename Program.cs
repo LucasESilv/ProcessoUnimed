df926415fa8d762db95857e750ee9cce9ff42a75
LucaseSilva <104096890+LucasESilv@users.noreply.github.com>
2023-11-28 21:45:07 -0300

    Adição de comentários na main

--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@ namespace ProcessoUnimed
     {
         static void Main(string[] args)
         {
+            //Sequencia de Fibonacci
             ProcessoUnimed.FibonacciSequence.FibonacciSequence fibonacci = new ProcessoUnimed.FibonacciSequence.FibonacciSequence();
             int n = 15;
             fibonacci.PrintFibonacci(n);
             
+            //Algoritmo de Ordenação, utilizei o mais utilizado que é o bubble sort
             int[] arrayToSort = { 64, 34, 25, 12, 22, 11, 90 };
             ProcessoUnimed.SimpleOrdering.SimpleOrdering sorter = new ProcessoUnimed.SimpleOrdering.SimpleOrdering();
             Console.WriteLine("Array antes da ordenação:");
@@ -18,6 +20,7 @@ namespace ProcessoUnimed
             Console.WriteLine("Array após a ordenação:");
             sorter.PrintArray(arrayToSort);
 
+            //Verificação de palíndromos 
             Console.WriteLine("Digite a palavra para sabermos se é um palíndromo");
             string input = Console.ReadLine();
             ProcessoUnimed.PalindromeChecker.PalindromeChecker checker = new ProcessoUnimed.PalindromeChecker.PalindromeChecker();
